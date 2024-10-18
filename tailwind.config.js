9970df337bf501015ba154c0ea3b7e6f4f8ce896
andi2631 <acorrotti@gmail.com>
2024-10-18 12:29:42 -0300

    feature: add pantone to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pantone: {
+          // Example: Pantone 19-4052 Classic Blue
+          'classic-blue': '#0F4C81',
+          // You can add more Pantone colors here
+        },
+      },
+    },
   },
   plugins: [],
-};
+}
