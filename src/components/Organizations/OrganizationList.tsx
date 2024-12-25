33c4c9f2ff42782604da74258d70b29b4a7d3cd5
andi2631 <acorrotti@gmail.com>
2024-12-25 12:06:24 -0300

    refactor: separacion organizaciones de interes

--- src/components/Organizations/OrganizationList.tsx
+++ src/components/Organizations/OrganizationList.tsx
@@ -1,37 +1,16 @@
 import React from "react";
 import OrganizationCard from "./OrgCard";
+import { RELATED_ONG } from "../../utils/relatedOrganizations";
 
-interface Organization {
-  imageUrl: string;
-  name: string;
-  link: string;
-}
+const relatedOrganizationsList = RELATED_ONG;
 
-const organizations: Organization[] = [
-  {
-    imageUrl: "https://via.placeholder.com/150",
-    name: "Organización 1",
-    link: "https://example.com/organization1",
-  },
-  {
-    imageUrl: "https://via.placeholder.com/150",
-    name: "Organización 2",
-    link: "https://example.com/organization2",
-  },
-  {
-    imageUrl: "https://via.placeholder.com/150",
-    name: "Organización 3",
-    link: "https://example.com/organization3",
-  },
-];
 
 const OrganizationList: React.FC = () => {
   return (
     <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mt-8">
       <h2 className="text-xl font-bold mb-4">Organizaciones de interés:</h2>
-      {/* Render the organization cards */}
       <div className="flex flex-wrap gap-4 mt-8">
-        {organizations.map((org, index) => (
+        {relatedOrganizationsList.map((org, index) => (
           <OrganizationCard
             key={index}
             imageUrl={org.imageUrl}
---
+++ src/utils/relatedOrganizations.ts
@@ -0,0 +1,23 @@
+interface Organization {
+  imageUrl: string;
+  name: string;
+  link: string;
+}
+
+export const RELATED_ONG: Organization[] = [
+  {
+    imageUrl: "https://via.placeholder.com/150",
+    name: "Organización 1",
+    link: "https://example.com/organization1",
+  },
+  {
+    imageUrl: "https://via.placeholder.com/150",
+    name: "Organización 2",
+    link: "https://example.com/organization2",
+  },
+  {
+    imageUrl: "https://via.placeholder.com/150",
+    name: "Organización 3",
+    link: "https://example.com/organization3",
+  },
+];
