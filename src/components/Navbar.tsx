7b06d24b78dbfcb71ae9e30d46ac2d09e2004b17
Andres Corrotti <acorrotti@gmail.com`>
2024-12-27 23:17:44 -0300

    feat: Design changes
    Change navbar text near photo and hero text

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
-            <a href="#" onClick={() => navigate('/')} className="text-xl font-lexend font-bold text-gray-800"> INSTITUTO DE CIENCIAS FAMILIARES</a>
+            <a href="#" onClick={() => navigate('/')} className="text-3xl font-lexend font-bold text-gray-800"> ICF </a>
             <img
               onClick={() => navigate('/')}
               src="https://instituto-familia-s3.s3.us-west-2.amazonaws.com/Instituto+de+Ciencias+Familiares-26.png"
