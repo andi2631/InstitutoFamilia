3c3fc6221afcd085b12913523bc0802d3e7454ad
Andres Corrotti <acorrotti@gmail.com`>
2026-04-17 12:22:28 -0300

    feat: cambios sobre roca

--- src/components/Events/SobreRocas/sobreRocasDescription.tsx
+++ src/components/Events/SobreRocas/sobreRocasDescription.tsx
@@ -4,12 +4,12 @@ const SobreRocasDescription: React.FC = () => {
   return (
     <div className="text-lg text-gray-700 leading-relaxed">
       <p className="my-2">
-        <strong>Sobre Roca: Cuatro Citas y un Retiro de dos.
+        <strong>Sobre Roca: Tres Citas y un Retiro de dos.
           ¡Un antes y un después!
         </strong>
       </p>
       <p>
-        Una oportunidad para fortalecer y profundizar la conexión en tu matrimonio. Cuatro Citas + un Retiro de fin de semana que te van a dar herramientas clave para crecer como pareja.
+        Una oportunidad para fortalecer y profundizar la conexión en tu matrimonio. Tres Citas + un Retiro de fin de semana que te van a dar herramientas clave para crecer como pareja.
       </p>
 
       <p className="mt-2">
@@ -23,12 +23,11 @@ const SobreRocasDescription: React.FC = () => {
         <li>El Retiro es un fin de semana donde van a profundizar y darle un impulso nuevo a su relación.</li>
       </ul>
 
-      <h3 className="text-xl font-semibold mt-4">Fechas y horarios:</h3>
       <p className="mt-2">
-        A definir...
+        Para más información hacé clic en el formulario de preinscripción
       </p>
       <h3 className="text-xl font-semibold mt-4">
-        Formulario de inscripción 2026: <a href="https://forms.gle/SfeDoccMgu6MQsb79" className="text-blue-600 underline"> Click aquí</a>
+        Formulario de preinscripción 2026: <a href="https://forms.gle/SfeDoccMgu6MQsb79" className="text-blue-600 underline"> Click aquí</a>
       </h3>
       <div className="flex items-center space-x-4 mt-4">
         <a href="https://www.instagram.com/sobreroca_matrimonios/" target="_blank" rel="noopener noreferrer">
